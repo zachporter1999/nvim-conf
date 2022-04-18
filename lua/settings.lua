70bdc87d94bbec83fa9899ef1d529070b8c2e14c
Zach Porter <zach.ld.porter@gmail.com>
2022-04-17 22:15:34 -0400

    Updated to use built in term over floaterm

--- lua/settings.lua
+++ lua/settings.lua
@@ -27,6 +27,13 @@ vim.cmd [[silent! set nu]]
 vim.g.mapleader = ' '
 
 vim.api.nvim_set_keymap('n', "<leader>n",  ":enew | edit ", {noremap = true})
-vim.api.nvim_set_keymap('n', "<leader>e",  ":!pandoc -o ",  {noremap = true})
+-- vim.api.nvim_set_keymap('n', "<leader>e",  ":!pandoc -o ",  {noremap = true})
 vim.api.nvim_set_keymap('t', "<esc><esc>", "<C-\\><C-n>",   {noremap = true})
 vim.api.nvim_set_keymap('n', "<leader>cd", ":cd %:p:h<cr>", {noremap = true})
+vim.api.nvim_set_keymap('n', "<leader><enter>", ":pwd<cr>", {noremap = true})
+
+vim.api.nvim_set_keymap('n', "<leader>tf", ":vsplit | term vifm<cr>i", {noremap = true})
+vim.api.nvim_set_keymap('n', "<leader>tv", ":vsplit | term<cr>i", {noremap = true})
+vim.api.nvim_set_keymap('n', "<leader>ts", ":split | term<cr><C-w><S-j>10<C-w>-i", {noremap = true})
+vim.api.nvim_set_keymap('n', "<leader>tt", ":tabnew | term<cr>i", {noremap = true})
+vim.api.nvim_set_keymap('n', "<leader>e", ":NERDTree<cr>", {noremap = true})
