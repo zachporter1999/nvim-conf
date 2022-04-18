824bfff54e1956736fa1314f158287ec93162eb0
Zach Porter <zach.ld.porter@gmail.com>
2022-04-17 22:05:17 -0400

    Removed org mode plugin config.

--- init.lua
+++ init.lua
@@ -28,20 +28,3 @@ require('plugins/which-key')
 vim.b.ale_linters = {'clangd', 'rust-analyzer'}
 -- vim.cmd 'set shell=powershell'
 
-require('orgmode').setup_ts_grammar(0)
-
-require'nvim-treesitter.configs'.setup {
-
-	highlight = {
-		enable = true,
-		deiable = {'org'},
-		additional_vim_regex_highlighting = {'org'},
-	},
-	ensure_installed = {'org'}
-}
-
-
-require('orgmode').setup({
-	org_agenda_files = {'~/org/*'},
-	org_default_notes_file = '~/org/notes.org',
-})
