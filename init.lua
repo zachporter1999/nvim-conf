5cf7603c90c55c86ab6fcb3ca4b65ac5b0113b2f
Zach Porter <zach.ld.porter@gmail.com>
2022-04-24 17:23:23 -0400

    Updated init with os check, and temporary lsp config

--- init.lua
+++ init.lua
@@ -15,16 +15,36 @@ require('plugins')
 -- --------------------
 -- General settings
 -- --------------------
-require('settings')
+require('conf/settings')
+require('conf/helpers')
 
 -- --------------------
 -- Plugin settings
 -- --------------------
-require('plugins/startify')
-require('plugins/telescope') 
-require('plugins/floaterm') 
-require('plugins/which-key')
+require('conf/plugins/startify')
+require('conf/plugins/telescope')
+require('conf/plugins/which-key')
 
-vim.b.ale_linters = {'clangd', 'rust-analyzer'}
--- vim.cmd 'set shell=powershell'
+-- Windows Specific
+local osType = getOSType()
+if (osType == 'win32') then
+	vim.cmd 'set shell=powershell'
+end
 
+local lsp_installer = require("nvim-lsp-installer")
+
+-- Register a handler that will be called for each installed server when it's ready (i.e. when installation is finished
+-- or if the server is already installed).
+lsp_installer.on_server_ready(function(server)
+    local opts = {}
+
+    -- (optional) Customize the options passed to the server
+    -- if server.name == "tsserver" then
+    --     opts.root_dir = function() ... end
+    -- end
+
+    -- This setup() function will take the provided server configuration and decorate it with the necessary properties
+    -- before passing it onwards to lspconfig.
+    -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+    server:setup(opts)
+end)
