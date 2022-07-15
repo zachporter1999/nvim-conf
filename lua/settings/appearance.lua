6f228c1a17daf86b78853fc68580c782c882c604
Zach Porter <zach.ld.porter@gmail.com>
2022-07-14 20:38:03 -0400

    remove onedark and vscode config

--- lua/settings/appearance.lua
+++ lua/settings/appearance.lua
@@ -5,40 +5,8 @@
 -- This config file deals with anything related to 
 -- the appearance of neovim.
 --]]
-local onedark = require('onedark')
-local lualine = require('lualine')
-        
--- ========================================
--- Lua Line
---
--- Setup up the tabline (top), and lualine (bottom).
--- ========================================
-lualine.setup({
-	option = {
-        icons_enabled = true,
-		theme         = "auto",
-	},
-    tabline = {
-      lualine_a = {}, 
-      lualine_b = {'windows'},
-      lualine_c = {},
-      lualine_x = {},
-      lualine_y = {'tabs'},
-      lualine_z = {},
-    },
-    extenstions = {},
-})
 
--- ========================================
--- Color Scheme
--- ========================================
-onedark.setup {
-    style = 'darker',
-    transparent = false,
-}
-onedark.load()
-
-vim.cmd [[silent! colorscheme onedark]]
+vim.cmd [[silent! colorscheme default]]
 
 -- ========================================
 -- Formatting
@@ -50,6 +18,4 @@ vim.o.autoindent     = true
 vim.o.expandtab      = true
 vim.o.number         = true
 vim.o.relativenumber = true
-vim.o.cursorline     = true
-vim.o.cursorcolumn   = true
 
