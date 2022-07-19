03d86d75aacdb50f1d6c82687ac4fb270f58788f
Zach Porter <zach.ld.porter@gmail.com>
2022-07-19 08:51:53 -0400

    Update nvim

--- lua/settings/appearance.lua
+++ lua/settings/appearance.lua
@@ -18,4 +18,10 @@ vim.o.autoindent     = true
 vim.o.expandtab      = true
 vim.o.number         = true
 vim.o.relativenumber = true
+vim.o.list           = true
 
+vim.cmd[[hi StatusLine   ctermfg=black  ctermbg=yellow]]
+vim.cmd[[hi StatusLineNC ctermfg=black  ctermbg=grey]]
+vim.cmd[[hi TabLine      ctermfg=yellow ctermbg=black]]
+vim.cmd[[hi TabLineFill  ctermfg=grey  ctermbg=black]]
+vim.cmd[[hi TabLineSel   ctermfg=yellow  ctermbg=black]]
