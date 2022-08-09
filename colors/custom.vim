93521f850885cd74300ba7b3f95d8cdb1f4c6e0a
Zach Porter <zach.ld.porter@gmail.com>
2022-08-08 20:47:23 -0400

    Added custom color scheme. Cleanup

---
+++ colors/custom.vim
@@ -0,0 +1,11 @@
+" Custom colourscheme
+" By: Zach Porter
+
+hi StatusLine   cterm=bold      ctermfg=4 ctermbg=0
+hi StatusLineNC                 ctermfg=0 ctermbg=8
+
+hi WinSeparator                 ctermfg=8 ctermbg=0
+
+hi TabLine      cterm=none      ctermfg=8 ctermbg=0
+hi TabLineFill  cterm=reverse   ctermfg=0 ctermbg=15
+hi TabLineSel   cterm=underline ctermfg=4 ctermbg=0
--- lua/settings/appearance.lua
+++ lua/settings/appearance.lua
@@ -6,27 +6,16 @@
 -- the appearance of neovim.
 --]]
 
-vim.cmd [[silent! colorscheme default]]
+vim.cmd [[silent! colorscheme custom]]
 
 -- ========================================
 -- Formatting
 -- ========================================
 vim.o.tabstop        = 4
 vim.o.shiftwidth     = 4
-vim.o.autochdir      = true
 vim.o.autoindent     = true
 vim.o.expandtab      = true
 vim.o.number         = true
 vim.o.relativenumber = true
 vim.o.list           = true
 
--- ========================================
--- Custom Colorscheme
--- TODO move to colour scheme
--- ========================================
-vim.cmd[[hi StatusLine   ctermfg=red  ctermbg=black]]
-vim.cmd[[hi StatusLineNC ctermfg=black  ctermbg=8]]
-vim.cmd[[hi WinSeparator ctermfg=red  ctermbg=black]]
-vim.cmd[[hi TabLine      ctermfg=8 ctermbg=black]]
-vim.cmd[[hi TabLineFill  ctermfg=black  ctermbg=white]]
-vim.cmd[[hi TabLineSel   ctermfg=red  ctermbg=black]]
