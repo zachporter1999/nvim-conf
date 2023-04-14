99aaa962369997bc2305f48c59c1c33e69822be4
Zachary Porter <zporter@blackberry.com>
2023-04-14 16:19:40 -0400

    Consolidate into init.lua + keymap cleanup

--- init.lua
+++ init.lua
@@ -1,109 +1,259 @@
 --[[
--- init.lua
--- by: Zach Porter
---
--- This is the main config file for my neovim config. A list of plugins, tools mappings, and loading of
--- the required settings files can be found here
---
+	init.lua
+	by: Zach Porter
+
+	This is the main config file for my neovim config. A list of plugins, tools mappings, and loading of
+	the required settings files can be found here
 --]]
 
--- ========================================
--- Packer Bootstrap
---
--- This is for installing packer when installing
--- neovim for the first time.
--- ========================================
+--[[
+	Packer Bootstrap
+
+	This is for installing packer when installing
+	neovim for the first time.
+--]]
 local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-    vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd 'packadd packer.nvim'
+	vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+	vim.cmd 'packadd packer.nvim'
 end
 
--- ========================================
--- Packer Load Plugins
---
--- Any plugins will be specified in this section.
--- ========================================
+--[[
+	Packer Load Plugins
+
+	Any plugins will be specified in this section.
+--]]
 local packer = require 'packer'
 
 function packer_plugins()
-    use 'wbthomason/packer.nvim'
+	use 'wbthomason/packer.nvim'
 
-    use 'mhinz/vim-startify'
-    use {
-	    'nvim-lualine/lualine.nvim',
-	    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-    }
-    use 'vimwiki/vimwiki'
+	-- Appearance
+	use 'ellisonleao/gruvbox.nvim'
+	-- Bloat?? doesnt hurt... 
+	use {
+		'nvim-lualine/lualine.nvim',
+		requires = {
+			'kyazdani42/nvim-web-devicons',
+			opt = true
+		}
+	}
 
-    use 'neovim/nvim-lspconfig'
-    use 'hrsh7th/cmp-nvim-lsp'
-    use 'hrsh7th/cmp-buffer'
-    use 'hrsh7th/cmp-path'
-    use 'hrsh7th/cmp-cmdline'
-    use 'hrsh7th/nvim-cmp'
-    use 'hrsh7th/cmp-vsnip'
-    use 'neovim/nvim-lspconfig'
-    use 'williamboman/nvim-lsp-installer'
+	-- Note taking and ToDo
+	use 'vimwiki/vimwiki'
+	use 'dhruvasagar/vim-dotoo'
 
-    use 'weirongxu/plantuml-previewer.vim'
-    use 'aklt/plantuml-syntax'
-    use 'tyru/open-browser.vim'
+	-- Plantuml
+	use 'weirongxu/plantuml-previewer.vim'
+	use 'aklt/plantuml-syntax'
+	use 'tyru/open-browser.vim'
 
+	-- Fuzzy Find
+	-- Leave for now... bloat??
+	use {
+		'nvim-telescope/telescope.nvim',
+		requires = {{'nvim-lua/plenary.nvim'}}
+	}
 
-    use {
-        'nvim-telescope/telescope.nvim',
-        requires = {{'nvim-lua/plenary.nvim'}}
-    }
-    use {'nvim-treesitter/nvim-treesitter'}
-    use {'nvim-orgmode/orgmode', config = function()
-      require('orgmode').setup{}
-    end
-    }
-
-    use {
-        'kyazdani42/nvim-tree.lua',
-        requires = {
-            'kyazdani42/nvim-web-devicons', -- optional, for file icons
-        },
-        -- tag = 'nightly' -- optional, updated every week. (see issue #1193)
-    }
-    use {
+	-- Which Key
+	-- Leave for now.. Telescope search for keys
+	use {
 	'folke/which-key.nvim',
-        config = function()
-            require("which-key").setup {}
-        end
-    }
+		config = function()
+			require("which-key").setup {}
+		end
+	}
 end
 
 packer.reset()
 packer.startup(packer_plugins)
 
--- ========================================
--- Load Settings
---
--- The configuration has been modularized and grouped
--- into easier to manager sections. Below are the settings
--- files that are being used.
---
--- Note: Any file in settings/ is a settings file and it 
---       tracked by git. 
--- Note: Any file in custom/ other than custom.lua will not 
---       be tracked by git and should be used to add Settings
---       unique to your system.
--- ========================================
+-- Set leader key
+vim.g.mapleader = ';'
 
-require("general")
-require("file-explorer")
-require("appearance")
-require("lsp")
-require("startmenu")
-require("custom")
+--[[
+	Appearance
+--]]
+vim.cmd [[ silent! colorscheme gruvbox ]]
+vim.o.background	= "light"
+vim.o.termguicolors	= true
 
--- ========================================
--- Combine custom lists
---
--- Any custom lists will need to be combined with the 
--- list that they extend.
--- ========================================
-bookmarks_load(custom_bookmarks)
+require('lualine').setup({
+	options = {
+		-- theme = "gruvbox",
+		theme = "auto",
+	},
+	tabline = {
+		lualine_a = {},
+		lualine_b = {'buffers'},
+		lualine_c = {},
+		lualine_x = {},
+		lualine_y = {'tabs'},
+		lualine_z = {}
+	}
+})
+
+--[[
+	Formatting
+--]]
+vim.o.tabstop		= 8
+vim.o.shiftwidth	= 8
+vim.o.autoindent	= true
+vim.o.expandtab	  	= false
+vim.o.number		= true
+vim.o.relativenumber 	= true
+vim.o.list		= true
+vim.o.wrap		= false
+
+--[[
+	DoToo
+--]]
+vim.g.dotoo_agenda_files = {
+	"~/dotoo/*.dotoo",
+}
+
+vim.g.dotoo_begin_src_languages={
+	"c",
+	"rust",
+	"vim",
+	"bash",
+}
+
+-- map these files to the underlying vimscript list.
+vim.cmd [[ let g:dotoo#agenda#files=g:dotoo_agenda_files ]]
+
+--[[
+	File Exploration
+--]]
+local wk = require("which-key")
+wk.register({
+	f = {
+		-- File navigation
+		name = "Open File",
+		N = {"New Buffer (Netrw)"},
+		T = {"New Tab (Netrw)"},
+
+		n = {"New Buffer (Telescope)"},
+		j = {"New Split Down (Telescope)"},
+		k = {"New Split Up (Telescope)"},
+		h = {"New Vsplit Left (Telescope)"},
+		l = {"New Vsplit Right (Telescope)"},
+		t = {"New Tab (Telescope)"},
+
+		g = {"Lve Grep"},
+	},
+	b = {
+		-- File navigation
+		name = "Open Buffer",
+		n = {"Switch to new buffer"},
+		j = {"Split down and open existing buffer"},
+		k = {"Split up and open existing buffer"},
+		h = {"Split left and open existing buffer"},
+		l = {"Split right and open existing buffer"},
+		t = {"Open buffer in new tab"},
+	},
+	g = {"Lve Grep"},
+}, { prefix = "<leader>"})
+
+--[[
+	File Explorer
+	Open new files using nvim tree
+--]]
+
+keymap={
+	-- Netrw based
+	{
+		mode='n',
+		keymap='<leader>fN',
+		command=':Lexplore<cr>',
+	},
+	{
+		mode='n',
+		keymap='<leader>fT',
+		command=':Texplore<cr>',
+	},
+	-- Telescope based
+	{
+		mode='n',
+		keymap='<leader>fn',
+		command=':Telescope find_files<cr>',
+	},
+	{
+		mode='n',
+		keymap='<leader>ft',
+		command=':tabnew | Telescope find_files<cr>',
+	},
+	-- Split Files
+	{
+		mode='n',
+		keymap="<leader>fk",
+		command=":set nosplitbelow  | split  | Telescope find_files<cr>",
+	},
+	{
+		mode='n',
+		keymap="<leader>fj",
+		command=":set   splitbelow  | split  | Telescope find_files<cr>",
+	},
+	{
+		mode='n',
+		keymap="<leader>fh",
+		command=":set nosplitright  | vsplit | Telescope find_files<cr>",
+	},
+	{
+		mode='n',
+		keymap="<leader>fl",
+		command=":set   splitright  | vsplit | Telescope find_files<cr>",
+	},
+	-- Split Buffers
+	{
+		mode='n',
+		keymap="<leader>bn",
+		command=":Telescope buffers<cr>",
+	},
+	{
+		mode='n',
+		keymap="<leader>bj",
+		command=":set nosplitbelow  | split  | Telescope buffers<cr>",
+	},
+	{
+		mode='n',
+		keymap="<leader>bj",
+		command=":set   splitbelow  | split  | Telescope buffers<cr>",
+	},
+	{
+		mode='n',
+		keymap="<leader>bh",
+		command=":set nosplitright  | vsplit | Telescope buffers<cr>",
+	},
+	{
+		mode='n',
+		keymap="<leader>bl",
+		command=":set   splitright  | vsplit | Telescope buffers<cr>",
+	},
+	{
+		mode='n',
+		keymap="<leader>bt",
+		command=":tabnew | Telescope buffers<cr>",
+	},
+	-- Searching
+	{
+		mode='n',
+		keymap="<leader>g",
+		command=":Telescope current_buffer_fuzzy_find<cr>",
+	},
+	{
+		mode='n',
+		keymap="<leader>G",
+		command=":Telescope live_grep<cr>",
+	},
+	{
+		mode='n',
+		keymap="<leader>k",
+		command=":Telescope keymaps<cr>",
+	},
+}
+
+for ignore, keymapping in pairs(keymap) do
+	vim.api.nvim_set_keymap( keymapping.mode, keymapping.keymap, keymapping.command, {noremap = true} )
+end
