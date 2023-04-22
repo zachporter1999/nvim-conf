5d1ee9c0c1e045609be3bb15ff21d6299cfc5487
Zach Porter <zach.ld.porter@gmail.com>
2023-04-22 16:16:51 -0400

    Cleanup and fix background

--- init.lua
+++ init.lua
@@ -30,8 +30,6 @@ function packer_plugins()
 	use 'wbthomason/packer.nvim'
 
 	-- Appearance
-	use 'ellisonleao/gruvbox.nvim'
-	-- Bloat?? doesnt hurt... 
 	use {
 		'nvim-lualine/lualine.nvim',
 		requires = {
@@ -41,20 +39,12 @@ function packer_plugins()
 	}
 
 	-- Note taking and ToDo
-	use 'vimwiki/vimwiki'
 	use 'dhruvasagar/vim-dotoo'
 
 	-- Plantuml
 	use 'weirongxu/plantuml-previewer.vim'
 	use 'aklt/plantuml-syntax'
 	use 'tyru/open-browser.vim'
-
-	-- Fuzzy Find
-	-- Leave for now... bloat??
-	use {
-		'nvim-telescope/telescope.nvim',
-		requires = {{'nvim-lua/plenary.nvim'}}
-	}
 
 	-- Which Key
 	-- Leave for now.. Telescope search for keys
@@ -75,24 +65,25 @@ vim.g.mapleader = ';'
 --[[
 	Appearance
 --]]
-vim.cmd [[ silent! colorscheme gruvbox ]]
-vim.o.background	= "light"
-vim.o.termguicolors	= true
+vim.cmd [[ silent! colorscheme default ]]
+vim.o.background	= "dark"
+vim.o.termguicolors	= false
 
-require('lualine').setup({
-	options = {
-		-- theme = "gruvbox",
-		theme = "auto",
-	},
-	tabline = {
-		lualine_a = {},
-		lualine_b = {'buffers'},
-		lualine_c = {},
-		lualine_x = {},
-		lualine_y = {'tabs'},
-		lualine_z = {}
-	}
-})
+if os.getenv("DISPLAY") then
+	require('lualine').setup({
+		options = {
+			theme = "nord",
+		},
+		tabline = {
+			lualine_a = {},
+			lualine_b = {'buffers'},
+			lualine_c = {},
+			lualine_x = {},
+			lualine_y = {'tabs'},
+			lualine_z = {}
+		}
+	})
+end
 
 --[[
 	Formatting
@@ -128,132 +119,7 @@ vim.cmd [[ let g:dotoo#agenda#files=g:dotoo_agenda_files ]]
 --]]
 local wk = require("which-key")
 wk.register({
-	f = {
-		-- File navigation
-		name = "Open File",
-		N = {"New Buffer (Netrw)"},
-		T = {"New Tab (Netrw)"},
-
-		n = {"New Buffer (Telescope)"},
-		j = {"New Split Down (Telescope)"},
-		k = {"New Split Up (Telescope)"},
-		h = {"New Vsplit Left (Telescope)"},
-		l = {"New Vsplit Right (Telescope)"},
-		t = {"New Tab (Telescope)"},
-
-		g = {"Lve Grep"},
-	},
-	b = {
-		-- File navigation
-		name = "Open Buffer",
-		n = {"Switch to new buffer"},
-		j = {"Split down and open existing buffer"},
-		k = {"Split up and open existing buffer"},
-		h = {"Split left and open existing buffer"},
-		l = {"Split right and open existing buffer"},
-		t = {"Open buffer in new tab"},
-	},
-	g = {"Lve Grep"},
+	c = { "Open Config" }
 }, { prefix = "<leader>"})
 
---[[
-	File Explorer
-	Open new files using nvim tree
---]]
-
-keymap={
-	-- Netrw based
-	{
-		mode='n',
-		keymap='<leader>fN',
-		command=':Lexplore<cr>',
-	},
-	{
-		mode='n',
-		keymap='<leader>fT',
-		command=':Texplore<cr>',
-	},
-	-- Telescope based
-	{
-		mode='n',
-		keymap='<leader>fn',
-		command=':Telescope find_files<cr>',
-	},
-	{
-		mode='n',
-		keymap='<leader>ft',
-		command=':tabnew | Telescope find_files<cr>',
-	},
-	-- Split Files
-	{
-		mode='n',
-		keymap="<leader>fk",
-		command=":set nosplitbelow  | split  | Telescope find_files<cr>",
-	},
-	{
-		mode='n',
-		keymap="<leader>fj",
-		command=":set   splitbelow  | split  | Telescope find_files<cr>",
-	},
-	{
-		mode='n',
-		keymap="<leader>fh",
-		command=":set nosplitright  | vsplit | Telescope find_files<cr>",
-	},
-	{
-		mode='n',
-		keymap="<leader>fl",
-		command=":set   splitright  | vsplit | Telescope find_files<cr>",
-	},
-	-- Split Buffers
-	{
-		mode='n',
-		keymap="<leader>bn",
-		command=":Telescope buffers<cr>",
-	},
-	{
-		mode='n',
-		keymap="<leader>bj",
-		command=":set nosplitbelow  | split  | Telescope buffers<cr>",
-	},
-	{
-		mode='n',
-		keymap="<leader>bj",
-		command=":set   splitbelow  | split  | Telescope buffers<cr>",
-	},
-	{
-		mode='n',
-		keymap="<leader>bh",
-		command=":set nosplitright  | vsplit | Telescope buffers<cr>",
-	},
-	{
-		mode='n',
-		keymap="<leader>bl",
-		command=":set   splitright  | vsplit | Telescope buffers<cr>",
-	},
-	{
-		mode='n',
-		keymap="<leader>bt",
-		command=":tabnew | Telescope buffers<cr>",
-	},
-	-- Searching
-	{
-		mode='n',
-		keymap="<leader>g",
-		command=":Telescope current_buffer_fuzzy_find<cr>",
-	},
-	{
-		mode='n',
-		keymap="<leader>G",
-		command=":Telescope live_grep<cr>",
-	},
-	{
-		mode='n',
-		keymap="<leader>k",
-		command=":Telescope keymaps<cr>",
-	},
-}
-
-for ignore, keymapping in pairs(keymap) do
-	vim.api.nvim_set_keymap( keymapping.mode, keymapping.keymap, keymapping.command, {noremap = true} )
-end
+vim.api.nvim_set_keymap( "n", "<leader>c", ":e "..vim.cmd[[ call stdpath("config") ]].."init.lua<cr>", { noremap = true } )
